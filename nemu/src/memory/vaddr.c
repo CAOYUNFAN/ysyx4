9f7d1f3c962d3c3f087dd387da66c8d1648f6022
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-07 02:06:00 +0800

    >  compile NEMU ysyx_22040000 张三 Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  02:06:00 up  6:38,  1 user,  load average: 0.74, 0.51, 0.56

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -1,12 +1,13 @@
 #include <isa.h>
 #include <memory/paddr.h>
 
-word_t vaddr_ifetch(vaddr_t addr, int len) {
+word_t vaddr_read(vaddr_t addr, int len) {
   return paddr_read(addr, len);
 }
 
-word_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+
+inline word_t vaddr_ifetch(vaddr_t addr, int len) {
+  return vaddr_read(addr, len);
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
