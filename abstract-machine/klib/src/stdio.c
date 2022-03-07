9e117b856047726dac000961af9d88a5b101abb4
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-07 21:19:54 +0800

    >  compile NEMU ysyx_22040000 张三 Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:19:54 up  3:05,  1 user,  load average: 1.17, 1.11, 0.90

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,24 +5,165 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+static const char num_table[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+
+#define read_num(fmt,data) {\
+	while(*(fmt)>='0'&&*(fmt)<='9') data = (data) *10 +*(fmt) -'0',++fmt;\
+}
+
+inline static char* num_to_str(char *st,int d,bool positive){
+	if(d==0){*st++='0';return st;}
+	if(d>=0&&positive) {*st++='+';}
+	if(d<0){*st++='-';d=-d;}
+	char temp[30];
+	int top=0;
+	for(;d;d/=10) temp[++top]=d%10+'0';
+	while(top) *st++=temp[top--];
+	return st;
+}
+
+inline static char* unum_to_str(char *st,unsigned int d,int base){
+	if(d==0){*st++='0';return st;}
+	char temp[30];
+	int top=0;
+	for(;d;d/=base) temp[++top]=num_table[d%base];
+	while(top) *st++=temp[top--];
+	return st;
+}
+
+#define MAX_NUM_stdio (1<<20)
+static char temp[MAX_NUM_stdio];
+
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap; va_start(ap,fmt);
+	int len=vsprintf(temp,fmt,ap);
+	va_end(ap);
+	putstr(temp);
+	return len;
+}
+
+
+#define cao_arg(d,ap,ch) switch(ch){\
+ case 'l':d=va_arg(ap,intptr_t);break;\
+ case 'L':d=va_arg(ap,int64_t);break;\
+ case 'h':\
+ default: d=va_arg(ap,int);break;\
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  int d;char c;char *st=out;char* s;
+	while(*fmt){
+		if(*fmt!='%'){
+			*st++=*fmt++;
+			continue;
+		}
+		int tag;
+
+		enum flags_type{
+			notype,
+			zero=1,
+			left=2,
+			positive=4,
+			empty=8,
+			sharp=16
+		};
+		size_t flags=notype;
+		tag=0;
+		while(!tag){
+			switch (*(++fmt)){
+				case '-':flags|=left;break;
+				case '+':flags|=positive;break;
+				case ' ':flags|=empty;panic("Not Implemented or Error happens!");break;
+				case '#':flags|=sharp;panic("Not Implemented or Error happens!");break;
+				case '0':flags|=zero;break;
+				default:tag=1;break;
+			}
+		}
+
+		size_t width=0;
+		if(*fmt>='0'&&*fmt<='9') read_num(fmt,width)
+		else if(*fmt=='*'){
+			width=va_arg(ap,int);
+			if(width<0) flags|=left,width=-width;
+			++fmt;
+		};
+
+
+		if(*fmt=='.') panic("Not Support float or Error happens!");
+
+		char length=' ';
+		if(*fmt=='h'||*fmt=='l'||*fmt=='L'||*fmt=='Z'||*fmt=='z'){
+			length=*(fmt++);
+			if(length=='l'&&*fmt=='l'){
+				length='L';
+				++fmt;
+				panic("Not implemented or Error hapens");
+			}
+		}
+
+		static char temp2[MAX_NUM_stdio];
+		char* stt=temp2;
+		switch (*(fmt++)){
+			case 's':
+				s=va_arg(ap,char *);
+				for(char *ss=s;*ss;++ss,++stt) *stt=*ss;
+				break;
+			case 'd':
+				cao_arg(d,ap,length)
+				stt=num_to_str(stt,d,flags&positive);
+				break;
+			case 'c':
+				c=(char)va_arg(ap,int);
+				*stt++=c;
+				break;
+			case '%':
+				*stt++='%';
+				break;
+			case 'u':
+				cao_arg(d,ap,length)
+				stt=unum_to_str(stt,(unsigned)d,10);
+				break;
+			case 'o':
+				cao_arg(d,ap,length)
+				stt=unum_to_str(stt,(unsigned)d,8);
+				break;
+			case 'X': case 'x':case 'p':
+				cao_arg(d,ap,length)
+				stt=unum_to_str(stt,(unsigned)d,16);
+				break;
+			default:
+				panic("Not implemented or error happens");
+				break;
+		}
+		*stt='\0';
+
+		if(!(flags&left))
+			for(int i=0;i+strlen(temp2)<width;++i) *(st++)=(flags&zero?'0':' ');
+		strcpy(st,temp2);
+		st+=strlen(temp2);
+		if(flags&left)
+			for(int i=0;i+strlen(temp2)<width;++i) *(st++)=' ';
+	}
+	*st='\0';
+	return st-out;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap; va_start(ap,fmt);
+	return vsprintf(out,fmt,ap);
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap; va_start(ap,fmt);
+	return vsnprintf(out,n,fmt,ap);
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  int len=vsprintf(temp,fmt,ap);
+	strncpy(out,temp,n-1);
+	out[n-1]='\0';
+	return len;
 }
 
 #endif
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,43 +5,78 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t i=0;
+	for(;*s++;++i);
+	return i;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  char * dest=dst;
+  for(;*src;++dest,++src) *dest=*src;
+  *dest='\0';
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  size_t i;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dst[i] = src[i];
+	for ( ; i < n; i++)
+		dst[i] = '\0';
+	return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  char * dest=dst+strlen(dst);
+	for (;*src;++dest,++src)
+		*dest=*src;
+  *dest='\0';
+	return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  for(;*s1&&*s2;++s1,++s2) 
+  if(*s1!=*s2) return *s1-*s2;
+
+  return *s1-*s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  size_t i=0;
+  for(;*s1 && *s2 && i<n; ++s1,++s2)
+  if(*s1 != *s2) return *s1 - *s2;
+  if(i==n) return 0;
+  return *s1- *s2;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  unsigned char uc=c;
+	for(unsigned char *ss=s;n;--n,++ss) *ss=uc;
+	return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  if(dst<src){
+		char *a=dst;const char *b=src;
+		for(;n;n--,a++,b++) *a=*b;
+	}else{
+		char *a=(char*)dst+n-1;char *b=(char*)src+n-1;
+		for(;n;n--,a--,b--) *a=*b;
+	}
+	return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  char *a=out;const char *b=in;
+	for(;n;n--,a++,b++) *a=*b;
+	return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  const unsigned char *ss1=s1;
+  const unsigned char *ss2=s2;
+  for(;n;--n,++ss1,++ss2) if(*ss1!=*ss2) return *ss1<*ss2?-1:1;
+  return 0;
 }
 
 #endif
