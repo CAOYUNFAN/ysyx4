854dc47aa921581fc1f6f22dbf909657efc38a48
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-07 20:37:55 +0800

    >  compile NEMU ysyx_22040000 张三 Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:37:55 up  2:23,  1 user,  load average: 0.39, 0.67, 0.67

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -8,9 +8,35 @@ const char *regs[] = {
   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 
+#define Cao_show_reg(name,data) printf("%8s 0x%016lx %ld\n",name,data,data)
+
 void isa_reg_display() {
+	for(int i=0;i<32;++i) 
+	Cao_show_reg(regs[i],gpr(i));
+	Cao_show_reg("pc",cpu.pc);
+/*	Cao_show_reg("mcause",sr(0x342));
+	Cao_show_reg("mstatus",sr(0x300));
+	Cao_show_reg("mie",sr(0x304));
+	Cao_show_reg("mepc",sr(0x341));
+	Cao_show_reg("satp",sr(0x180));
+	Cao_show_reg("mscratch",sr(0x340));*/
+	return;
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
-  return 0;
+//	printf("%s\n",s+1);
+	for(int i=0;i<32;++i){
+//		printf("%s ",regs[i]);
+		if(strcmp(s+1,regs[i])==0){
+			*success=true;
+			return gpr(i);
+		}
+//		printf("%d\n",strcmp(s+1,regs[i]));
+	}
+	if(strcmp(s+1,"pc")==0){
+		*success=true;
+		return cpu.pc;
+	}
+	*success=false;
+	return 0;
 }
