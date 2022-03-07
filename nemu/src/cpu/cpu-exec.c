bd6752297ac1950f6f04ccf777e61d26ec89c5f1
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-07 20:43:48 +0800

    >  compile NEMU ysyx_22040000 张三 Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:43:48 up  2:29,  1 user,  load average: 1.92, 1.02, 0.77

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -23,6 +23,11 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+
+  #ifndef CONFIG_TARGET_AM
+  extern bool watchpoint_check();
+  if(watchpoint_check()) nemu_state.state=NEMU_STOP;
+  #endif
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
