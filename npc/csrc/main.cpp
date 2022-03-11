05d56c5236dd5412c26add1cb34f491098c4acf8
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-12 02:11:44 +0800

    >  compile NEMU ysyx_220066 曹云帆 Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  02:11:44 up  1:53,  1 user,  load average: 0.57, 0.46, 0.41

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include "emu.h"
+
 
 int main() {
   printf("Hello, ysyx!\n");
+  std::shared_ptr<emu> top = std::make_shared<emu>();
+  for(int i=0;i<1000;++i) {
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top->a = a;
+    top->b = b;
+    top->eval();
+    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+    assert(top->f == a ^ b);
+  }
   return 0;
 }
