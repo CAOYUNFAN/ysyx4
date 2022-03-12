d53d66f8df01ec2a9e061f2afcf168163e830e02
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-13 01:40:30 +0800

    >  compile NEMU ysyx_220066 曹云帆 Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  01:40:30 up 30 min,  1 user,  load average: 0.45, 0.35, 0.37

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -14,10 +14,16 @@ paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 static word_t pmem_read(paddr_t addr, int len) {
   word_t ret = host_read(guest_to_host(addr), len);
+  #ifdef CONFIG_MTRACE
+    Log("MTRACE_read:addr=%x,len=%d,data=%lu",addr,len,ret);
+  #endif
   return ret;
 }
 
 static void pmem_write(paddr_t addr, int len, word_t data) {
+  #ifdef CONFIG_MTRACE
+    Log("MTRACE_write:addr=%x,len=%d,data=%lu",addr,len,data);
+  #endif
   host_write(guest_to_host(addr), len, data);
 }
 
