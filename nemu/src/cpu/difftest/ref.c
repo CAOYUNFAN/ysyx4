9499da95bcac57b6875fe24911a2236d7e84225e
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-13 04:20:46 +0800

    >  compile NEMU ysyx_220066 曹云帆 Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  04:20:46 up  3:10,  1 user,  load average: 0.87, 0.99, 1.15

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -4,22 +4,18 @@
 #include <memory/paddr.h>
 
 void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  printf("hello1!\n");
   assert(0);
 }
 
 void difftest_regcpy(void *dut, bool direction) {
-  printf("hello2!\n");
   assert(0);
 }
 
 void difftest_exec(uint64_t n) {
-  printf("hello3!\n");
   assert(0);
 }
 
 void difftest_raise_intr(word_t NO) {
-  printf("hello4!\n");
   assert(0);
 }
 
