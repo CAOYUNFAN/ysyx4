25a90a198a0e614e6cd0c0fd29e27c1c5bf66385
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-13 05:22:36 +0800

    >  compile NEMU ysyx_220066 曹云帆 Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  05:22:36 up  4:12,  1 user,  load average: 0.90, 0.86, 0.83

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -2,6 +2,8 @@
 #include <nemu.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
+#define wide (uint32_t)inw(VGACTL_ADDR+2)
+#define high (uint32_t)inw(VGACTL_ADDR)
 
 void __am_gpu_init() {
 }
@@ -9,12 +11,17 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = wide, .height = high,
     .vmemsz = 0
   };
+  cfg->vmemsz=cfg->width*cfg->height*sizeof(uint32_t);
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t *data=ctl->pixels;
+  for(int i=0;i<ctl->h;i++)
+  for(int j=0;j<ctl->w;j++)
+  outl(((ctl->x+j)+wide*(ctl->y+i))*4+FB_ADDR,*(data++));
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
