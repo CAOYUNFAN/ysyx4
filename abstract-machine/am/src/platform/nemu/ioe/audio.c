7953b123ad68d3e6f1c234961d816819e1b6293d
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-15 01:27:38 +0800

    >  compile NEMU ysyx_220066 曹云帆 Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  01:27:38 up  5:22,  1 user,  load average: 1.23, 1.27, 1.14

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -36,10 +36,10 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
   int block_size=inl(AUDIO_SBUF_SIZE_ADDR);
   for(;ptr<end;){
     int count=inl(AUDIO_COUNT_ADDR);
-    printf("HELLO_WORLD%d-%d,%ld!\n",count,block_size,ptr);
+    printf("HELLO_WORLD%d-%d,%lx!\n",count,block_size,ptr);
     for(;ptr<end&&count<block_size;++ptr,++count) 
       outb(AUDIO_SBUF_ADDR+count,*((unsigned char *)ptr));
-    printf("COUNTAFTER:%d,%ld\n",count,ptr);
+    printf("COUNTAFTER:%d,%lx\n",count,ptr);
     outl(AUDIO_ADDR,count);
   }
 }
