fc43aa99320a1cacef9b874b242de242136a8135
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-09 03:12:34 +0800

    >  compile NEMU ysyx_220066 曹云帆 Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  03:12:34 up  2:24,  1 user,  load average: 0.82, 0.75, 0.72

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -4,11 +4,11 @@
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i=0;i<32;++i) if(cpu.gpr[i]!=ref_r->gpr[i]) {
-    Log("Difftest: different on reg[%d]",i);
+    Log("Difftest: different on reg[%d],ref=%lx",i,ref_r->gpr[i]);
     return false;
   }
   if(cpu.pc!=pc) {
-    Log("Difftest: different on pc");
+    Log("Difftest: different on pc,ref=%lx",pc);
     return false;
   }
   return true;
