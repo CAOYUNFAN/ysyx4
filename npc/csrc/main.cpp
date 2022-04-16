e342102d6d53673cd308cb260f1bb9d92344fe10
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-17 03:43:01 +0800

    >  sim RTL ysyx_220066 曹云帆 Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  03:43:01 up  1:21,  1 user,  load average: 1.69, 1.08, 0.92

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -31,7 +31,6 @@ void cpu_exec(uLL n){
 
 char * img_file=NULL;
 void parse_args(int argc,char * argv[]){
-  for(int i=0;i<argc;++i) printf("%s\n",argv[i]);
   static const option table[] ={
     {"batch"    , no_argument      , NULL, 'b'},
     {"log"      , required_argument, NULL, 'l'},
