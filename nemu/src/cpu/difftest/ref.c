7ce156c8b27074a7ae3700d5d08ac03ec3545abc
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-23 04:50:19 +0800

    >  compile NEMU ysyx_220066 曹云帆 Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  04:50:19 up  3:03,  1 user,  load average: 0.37, 0.47, 0.63

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -4,15 +4,27 @@
 #include <memory/paddr.h>
 
 void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  assert(0);
+  if(direction==DIFFTEST_TO_DUT){
+    memcpy(buf,guest_to_host(RESET_VECTOR),n);
+  }else{
+    Assert(direction==DIFFTEST_TO_REF,"Unexpected direction %d",direction);
+    memcpy(guest_to_host(RESET_VECTOR),buf,n);
+  }
+  return;
 }
 
 void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
+  if(direction==DIFFTEST_TO_DUT){
+    memcpy(dut,&cpu,sizeof(CPU_state));
+  }else{
+    Assert(direction==DIFFTEST_TO_REF,"Unexpected direction %d",direction);
+    memcpy(&cpu,dut,sizeof(CPU_state));
+  }
 }
 
 void difftest_exec(uint64_t n) {
-  assert(0);
+  if(nemu_state.state==NEMU_END||nemu_state.state==NEMU_ABORT) panic("REF HAS ENDED!\n");
+  cpu_exec(n);
 }
 
 void difftest_raise_intr(word_t NO) {
