f26b0b016e476ee6998f346d82ef286e504af701
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-24 07:41:23 +0800

    >  sim RTL ysyx_220066 曹云帆 Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  07:41:23 up  1:55,  1 user,  load average: 0.72, 0.70, 0.65

--- npc/csrc/include/common.h
+++ npc/csrc/include/common.h
@@ -21,7 +21,7 @@
 #define Assert(cond, format, ...) \
     do { \
         if (!(cond)) { \
-            fflush(stdout), fprintf(stderr, ASNI_FMT(format, ASNI_FG_RED) "\n", ##  __VA_ARGS__); \
+            fflush(stdout), fprintf(stderr, ASNI_FMT("[%s:%d %s]" format, ASNI_FG_RED) "\n", __FILE__,__LINE__,__func__,##  __VA_ARGS__); \
             assert(cond); \
         } \
     } while (0)
