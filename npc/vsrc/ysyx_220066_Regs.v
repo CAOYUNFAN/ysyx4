92128370f466fac476e5e8c608353cab492b25ad
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-24 09:42:12 +0800

    >  sim RTL ysyx_220066 曹云帆 Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  09:42:12 up  3:56,  1 user,  load average: 1.07, 0.92, 0.77

--- npc/vsrc/ysyx_220066_Regs.v
+++ npc/vsrc/ysyx_220066_Regs.v
@@ -7,7 +7,7 @@ module ysyx_220066_Regs #(ADDR_WIDTH = 5, DATA_WIDTH = 64) (
   reg [DATA_WIDTH-1:0] rf [31:0];
   always @(posedge clk) begin
     if (wen) begin 
-      $display("addr=%h,data=%h",waddr,wdata);
+      $display("in:addr=%h,data=%h",waddr,wdata);
       rf[waddr] <= wdata;
     end
   end
