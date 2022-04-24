6f5e56775ff41f1490fa61f6e4bdcfe51ccd607b
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-24 21:37:05 +0800

    >  sim RTL ysyx_220066 曹云帆 Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:37:05 up 50 min,  1 user,  load average: 0.15, 0.44, 0.52

--- npc/vsrc/ysyx_220066_ALU.v
+++ npc/vsrc/ysyx_220066_ALU.v
@@ -7,7 +7,7 @@ module ysyx_220066_ALU(
     );
     wire ALctr,SUBctr,SIGctr,Wctr,CF,SF,OF;
     wire [63:0] Add_result;
-    ysyx_220066_ALU_decode alu_decode(aluctr[4:3],ALctr,SUBctr,SIGctr,Wctr);
+    ysyx_220066_ALU_decode alu_decode(aluctr[4:2],ALctr,SUBctr,SIGctr,Wctr);
     wire [63:0] data;
     assign data[31:0]=data_input[31:0];
     assign data[63:32]=Wctr?{32{data_input[31]}}:data_input[63:32];
@@ -61,10 +61,10 @@ module ysyx_220066_Adder(
 endmodule
 
 module ysyx_220066_ALU_decode(
-    input [4:3] ALUctr,
+    input [4:2] ALUctr,
     output ALctr,SUBctr,SIGctr,Wctr
     );
-    assign SUBctr=ALUctr[3];
+    assign SUBctr=ALUctr[3]|ALUctr[2];
     assign ALctr=ALUctr[3];
     assign SIGctr=ALUctr[3];
     assign Wctr=ALUctr[4];
