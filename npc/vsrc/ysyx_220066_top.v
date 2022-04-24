80785eef71905f91aaa935376f54afaef15c5b28
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-24 09:17:11 +0800

    >  sim RTL ysyx_220066 曹云帆 Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  09:17:11 up  3:31,  1 user,  load average: 0.60, 0.58, 0.57

--- npc/vsrc/ysyx_220066_top.v
+++ npc/vsrc/ysyx_220066_top.v
@@ -44,7 +44,7 @@ module ysyx_220066_top(
     endcase
   end
 
-  always @(posedge clk) begin
+  always @(*) begin
     $display("addr=%h,addr_low=%b",addr,addr_[2:0]);
   end
 
@@ -55,28 +55,28 @@ module ysyx_220066_top(
   always @(*) begin
     case(MemOp)
       3'b000:begin
-        wmask[0]=(addr_[2:0]==3'o0);
-        wmask[1]=(addr_[2:0]==3'o1);
-        wmask[2]=(addr_[2:0]==3'o2);
-        wmask[3]=(addr_[2:0]==3'o3);
-        wmask[4]=(addr_[2:0]==3'o4);
-        wmask[5]=(addr_[2:0]==3'o5);
-        wmask[6]=(addr_[2:0]==3'o6);
-        wmask[7]=(addr_[2:0]==3'o7);
+        wmask[0]=(addr[2:0]==3'o0);
+        wmask[1]=(addr[2:0]==3'o1);
+        wmask[2]=(addr[2:0]==3'o2);
+        wmask[3]=(addr[2:0]==3'o3);
+        wmask[4]=(addr[2:0]==3'o4);
+        wmask[5]=(addr[2:0]==3'o5);
+        wmask[6]=(addr[2:0]==3'o6);
+        wmask[7]=(addr[2:0]==3'o7);
       end
       3'b001:begin
-        wmask[0]=(addr_[2:1]==2'o0);
-        wmask[1]=(addr_[2:1]==2'o0);
-        wmask[2]=(addr_[2:1]==2'o1);
-        wmask[3]=(addr_[2:1]==2'o1);
-        wmask[4]=(addr_[2:1]==2'o2);
-        wmask[5]=(addr_[2:1]==2'o2);
-        wmask[6]=(addr_[2:1]==2'o3);
-        wmask[7]=(addr_[2:1]==2'o3);
+        wmask[0]=(addr[2:1]==2'o0);
+        wmask[1]=(addr[2:1]==2'o0);
+        wmask[2]=(addr[2:1]==2'o1);
+        wmask[3]=(addr[2:1]==2'o1);
+        wmask[4]=(addr[2:1]==2'o2);
+        wmask[5]=(addr[2:1]==2'o2);
+        wmask[6]=(addr[2:1]==2'o3);
+        wmask[7]=(addr[2:1]==2'o3);
       end
       3'b010:begin
-        wmask[3:0]={4{addr_[2]}};
-        wmask[7:4]={4{addr_[2]}};
+        wmask[3:0]={4{addr[2]}};
+        wmask[7:4]={4{addr[2]}};
       end
       default: wmask=8'hff;
     endcase
