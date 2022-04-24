00216d1db163530f618981d959b32357066e972a
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-24 21:22:06 +0800

    >  sim RTL ysyx_220066 曹云帆 Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:22:06 up 40 min,  1 user,  load average: 0.50, 0.50, 0.52

--- npc/vsrc/ysyx_220066_ALU.v
+++ npc/vsrc/ysyx_220066_ALU.v
@@ -55,7 +55,7 @@ module ysyx_220066_Adder(
         {Cout,result[63]}={1'b0,x[63]}+{1'b0,y_[63]}+{1'b0,Ctemp};
         SF=result[63];
         OF=Cout^Ctemp;
-        ZF=~|result;
+        ZF=(result==64'h0);
         CF=SUBctr^Cout;
     end
 endmodule
