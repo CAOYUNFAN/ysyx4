37775cc1e7bce27aa62a4ab7846b78e51a2cfdb5
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-24 21:13:50 +0800

    >  sim RTL ysyx_220066 曹云帆 Linux cao-VirtualBox 5.13.0-19-generic #19-Ubuntu SMP Thu Oct 7 21:58:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:13:50 up 31 min,  1 user,  load average: 0.57, 0.58, 0.54

--- npc/vsrc/ysyx_220066_ID.v
+++ npc/vsrc/ysyx_220066_ID.v
@@ -58,7 +58,7 @@ module ysyx_220066_Decode (
     assign RegWr=(OP[6:2]!=5'b11000&&OP[6:2]!=5'b01000&&OP[6:2]!=5'b11100);
     assign ALUASrc=(OP[6:2]==5'b00101||OP[6:2]==5'b11011||OP[6:2]==5'b11001);
     reg [3:0] ALUctr;reg err;
-    assign ALUctr_out[5]=(OP[6:2]==5'b01110)||(OP[6:2]==5'b01100&&Funct7[0]);
+    assign ALUctr_out[5]=((OP[6:2]==5'b01110||OP[6:2]==5'b01100)&&Funct7[0]);
     assign ALUctr_out[4]=OP[3]&~OP[2];
     assign ALUctr_out[3:0]=ALUctr;
     assign done=(OP[6:2]==5'b11100);
