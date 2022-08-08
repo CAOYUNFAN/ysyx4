e2ef8f5c59363810b5d3f07bd0c3dd347e3c9444
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-08 23:34:06 +0800

    >  compile NEMU ysyx_220066 曹云帆 Linux cao-VirtualBox 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:34:06 up  1:03,  1 user,  load average: 0.88, 0.88, 0.89

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -2,6 +2,12 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
+#define CHECK(name) \
+if(cpu.name !=ref_r->name ){\
+  Log("Difftest: different on " str(name) ",ref=%lx",ref_r->name);\
+  return false;\
+}
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i=0;i<32;++i) if(cpu.gpr[i]!=ref_r->gpr[i]) {
     Log("Difftest: different on reg[%d],ref=%lx",i,ref_r->gpr[i]);
@@ -9,6 +15,24 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   }
   if(ref_r->pc!=pc) {
     Log("Difftest: different on pc,ref=%lx",ref_r->pc);
+    return false;
+  }
+  
+  CHECK(mepc)
+  CHECK(mstatus)
+  CHECK(mcause)
+  CHECK(mtvec)
+  
+  if(cpu.mepc!=ref_r->mepc) {
+    Log("Difftest: different on mepc,ref=%lx",ref_r->mepc);
+    return false;
+  }
+  if(cpu.mstatus!=ref_r->mepc) {
+    Log("Difftest: different on mepc,ref=%lx",ref_r->mepc);
+    return false;
+  }
+  if(cpu.mepc!=ref_r->mepc) {
+    Log("Difftest: different on mepc,ref=%lx",ref_r->mepc);
     return false;
   }
   return true;
