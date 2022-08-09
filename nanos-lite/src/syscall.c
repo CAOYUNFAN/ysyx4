249d12c2c1d8f797640f3bd4a745e976f1c9ba9c
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-09 22:49:24 +0800

    >  compile NEMU ysyx_220066 曹云帆 Linux cao-VirtualBox 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:49:24 up  3:10,  1 user,  load average: 0.76, 0.50, 0.41

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -18,14 +18,19 @@ size_t sys_write(int fd,void * buf,size_t count){
   return count; 
 }
 
+int sys_brk(void * addr){
+  return 0;
+}
+
 Context * do_syscall(Context *c) {
   uintptr_t a[4];
-  a[0] = c->GPR1;
+  a[0] = c->GPR1;a[1]=c->GPR2;a[2]=c->GPR3;a[3]=c->GPR4;
 
   switch (a[0]) {
     case SYS_yield: c=do_yield(c); break;
     case SYS_exit: c=do_exit(c); break;
-    case SYS_write: c->GPRx=sys_write((int)c->GPR2,(void *)c->GPR3,c->GPR4); break;
+    case SYS_write: c->GPRx=sys_write((int)a[1],(void *)a[2],a[3]); break;
+    case SYS_brk: c->GPRx=sys_brk((void *)a[1]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
   return c;
