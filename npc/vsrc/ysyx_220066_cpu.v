1feb2cba30d6628a739c33bd4f7f369af5aed4c8
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-12 01:37:22 +0800

    >  sim RTL ysyx_220066 曹云帆 Linux cao-VirtualBox 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:37:22 up  1:31,  1 user,  load average: 0.43, 0.51, 0.56

--- npc/vsrc/ysyx_220066_cpu.v
+++ npc/vsrc/ysyx_220066_cpu.v
@@ -86,7 +86,7 @@ module ysyx_220066_cpu(
     assign addr=alu_result;
 
     always @(*) if(!rst) begin
-//        $display("clk=%b,pc=%h,instr=%h,src1=%h,csr_wr_darta=%h",clk,pc,instr,src1,csr_wrdata);
+        $display("clk=%b,pc=%h,instr=%h",clk,pc,instr);
 //        if(clk) $display("iscsr?%b,Funct3=%b,csrwen=",iscsr,instr[14:12],csr_wen&&~error_temp);
     end
 endmodule
