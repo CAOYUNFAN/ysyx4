27b666c2b37d79dd4790a926205d27558f0d3609
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-12 01:45:13 +0800

    >  sim RTL ysyx_220066 曹云帆 Linux cao-VirtualBox 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:45:13 up  1:39,  1 user,  load average: 0.78, 0.67, 0.62

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,8 +24,8 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-//  extern void naive_uload(PCB *pcb, const char *filename);
-//  naive_uload(NULL,NULL);
+  extern void naive_uload(PCB *pcb, const char *filename);
+  naive_uload(NULL,NULL);
 }
 
 Context* schedule(Context *prev) {
