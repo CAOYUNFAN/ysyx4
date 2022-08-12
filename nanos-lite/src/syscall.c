0adec956717cfa38a2d5687fc3f26f30305e3689
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-13 03:19:49 +0800

    >  compile NEMU ysyx_220066 曹云帆 Linux cao-VirtualBox 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:19:49 up  4:38,  1 user,  load average: 1.13, 0.65, 0.70

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -16,6 +16,16 @@ int sys_brk(void * addr){
   return 0;
 }
 
+uintptr_t sys_gettimeofday(struct timeval *tv,struct timezone * tz){
+  uint64_t time=io_read(AM_TIMER_UPTIME).us;
+  if(tv){
+    tv->tv_sec=time/1000000;
+    tv->tv_usec=time;
+  }
+  if(tz) tz->tz_dsttime=tz->tz_minuteswest=0;
+  return 0;
+}
+
 Context * do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;a[1]=c->GPR2;a[2]=c->GPR3;a[3]=c->GPR4;
@@ -29,6 +39,7 @@ Context * do_syscall(Context *c) {
     case SYS_open: c->GPRx=fs_open((char *)a[1],a[2],a[3]); break;
     case SYS_lseek: c->GPRx=fs_lseek(a[1],a[2],a[3]); break;
     case SYS_close: c->GPRx=fs_close(a[1]); break;
+    case SYS_gettimeofday: c->GPRx=sys_gettimeofday((struct timeval *)a[1],(struct timezone *)a[2]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
   return c;
---
+++ navy-apps/tests/timer-test/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <NDL.h>
+#include <sys/time.h>
+#include <assert.h>
+
+#ifdef TIMER_USE_NDL
+#define gap 1000
+#define fmt "%d.%03d"
+#else
+#define gap 1000000
+#define fmt "%d.%06d"
+#endif
+
+int get_time(){
+    #ifdef TIMER_USE_NDL
+    return NDL_GetTicks();
+    #else
+    struct timeval tv;
+    assert(gettimeofday(&tv,NULL)==0);
+    return tv.tv_usec;
+    #endif
+}
+
+int main(){
+    printf("timer_test start!\n");
+    int now=get_time(),i=0;
+    while (1){
+        while(get_time()-now<gap);
+        now=get_time();
+        printf("timer test:%d time, " fmt,i,now/gap,now%gap);
+        i++;
+    }
+}
