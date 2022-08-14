81f2ebad434b08352b3ee4a8177549b6d1107fed
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-14 23:57:00 +0800

    >  compile NEMU ysyx_220066 曹云帆 Linux cao-VirtualBox 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:57:00 up 41 min,  1 user,  load average: 1.02, 0.93, 0.89

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -106,6 +106,7 @@ static inline void init_dispinfo(){
   int total=dispinfo.vmemsz;
   for(;total>=0;total-=sizeof(buf)){
     int size=sizeof(buf);if(size>total) size=total;
+    printf("print %d",size);
     fwrite(buf,1,size,dev_fb);
   }
   fflush(dev_fb);
