e66db335955d926c08f058d30db36159b66f287b
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-29 19:01:10 +0800

    >  sim RTL ysyx_220066 曹云帆 Linux cao-VirtualBox 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:01:10 up 42 min,  1 user,  load average: 0.43, 0.55, 0.51

--- npc/vsrc/CPU/EX/ysyx_220066_EX.v
+++ npc/vsrc/CPU/EX/ysyx_220066_EX.v
@@ -114,6 +114,6 @@ module ysyx_220066_EX(
     assign result=ALUctr_native[5]?result_line:mul_result;
 
     always @(*) begin
-        if(~rst&&~clk) $display("EX:nxtpc=%h,valid=%b,is_jmp=%b,is_csr=%b,MemWr=%b,error=%b",nxtpc,valid,is_jmp,csr_native,MemWr_native,error);
+        if(~rst&&~clk) $display("EX:nxtpc=%h,valid=%b,result=%b,is_jmp=%b,is_csr=%b,MemWr=%b,error=%b",nxtpc,valid,result,is_jmp,csr_native,MemWr_native,error);
     end
 endmodule
