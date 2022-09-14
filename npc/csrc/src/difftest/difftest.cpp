e86d1a918c614d49c0fcce7a6a14edcf0c7372b2
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-15 04:21:20 +0800

    >  sim RTL ysyx_220066 曹云帆 Linux cao-VirtualBox 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:21:20 up  3:27,  1 user,  load average: 0.58, 0.67, 0.86

--- npc/csrc/src/difftest/difftest.cpp
+++ npc/csrc/src/difftest/difftest.cpp
@@ -112,7 +112,7 @@ void difftest_step(uLL pc, uLL npc) {
     }
     ref_difftest_exec(1);
     ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
-    if(npc>=0x80001270&&npc<0x80001290) Log("npc:%llx,mtvec:%llx",npc,ref_r.mtvec);
+    if(pc>=0x80001270&&pc<0x80001290) Log("npc:%llx,mtvec:%llx",npc,ref_r.mtvec);
     checkregs(&ref_r, npc);
 }
 
