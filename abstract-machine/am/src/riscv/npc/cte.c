f61a0a3a6225cb4a82f77dce3bc8e297b329e2f3
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-16 07:18:36 +0800

    >  sim RTL ysyx_220066 曹云帆 Linux cao-VirtualBox 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:18:36 up  2:39,  1 user,  load average: 1.03, 1.03, 0.90

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -45,6 +45,6 @@ bool ienabled() {
 }
 
 void iset(bool enable) {
-  asm("csrw mstatus,%0" : :"r"(enable?0x00000000a0001888uLL:0x00000000a0001880uLL));
   asm("csrw mie,%0": :"r"(enable?0x80uLL:0));
+  asm("csrw mstatus,%0" : :"r"(enable?0x00000000a0001888uLL:0x00000000a0001880uLL));
 }
