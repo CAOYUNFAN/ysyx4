b3b8cb1182b974de5a3ec449305bd61daa1ae9ef
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-27 10:44:27 +0800

    >  compile NEMU ysyx_220066 曹云帆 Linux cao-VirtualBox 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:44:27 up 31 min,  1 user,  load average: 0.84, 0.72, 0.66

--- npc/vsrc/ysyx_040066.v
+++ npc/vsrc/ysyx_040066.v
@@ -2,13 +2,6 @@
 `define AXI_BURST_TYPE_FIXED                                2'b00               //突发类型  FIFO
 `define AXI_BURST_TYPE_INCR                                 2'b01               //ram  
 `define AXI_BURST_TYPE_WRAP                                 2'b10
-// Access permissions
-`define AXI_PROT_UNPRIVILEGED_ACCESS                        3'b000
-`define AXI_PROT_PRIVILEGED_ACCESS                          3'b001
-`define AXI_PROT_SECURE_ACCESS                              3'b000
-`define AXI_PROT_NON_SECURE_ACCESS                          3'b010
-`define AXI_PROT_DATA_ACCESS                                3'b000
-`define AXI_PROT_INSTRUCTION_ACCESS                         3'b100
 // Memory types (AR)
 `define AXI_ARCACHE_DEVICE_NON_BUFFERABLE                   4'b0000
 `define AXI_ARCACHE_DEVICE_BUFFERABLE                       4'b0001
@@ -22,19 +15,6 @@
 `define AXI_ARCACHE_WRITE_BACK_READ_ALLOCATE                4'b1111
 `define AXI_ARCACHE_WRITE_BACK_WRITE_ALLOCATE               4'b1011
 `define AXI_ARCACHE_WRITE_BACK_READ_AND_WRITE_ALLOCATE      4'b1111
-// Memory types (AW)
-`define AXI_AWCACHE_DEVICE_NON_BUFFERABLE                   4'b0000
-`define AXI_AWCACHE_DEVICE_BUFFERABLE                       4'b0001
-`define AXI_AWCACHE_NORMAL_NON_CACHEABLE_NON_BUFFERABLE     4'b0010
-`define AXI_AWCACHE_NORMAL_NON_CACHEABLE_BUFFERABLE         4'b0011
-`define AXI_AWCACHE_WRITE_THROUGH_NO_ALLOCATE               4'b0110
-`define AXI_AWCACHE_WRITE_THROUGH_READ_ALLOCATE             4'b0110
-`define AXI_AWCACHE_WRITE_THROUGH_WRITE_ALLOCATE            4'b1110
-`define AXI_AWCACHE_WRITE_THROUGH_READ_AND_WRITE_ALLOCATE   4'b1110
-`define AXI_AWCACHE_WRITE_BACK_NO_ALLOCATE                  4'b0111
-`define AXI_AWCACHE_WRITE_BACK_READ_ALLOCATE                4'b0111
-`define AXI_AWCACHE_WRITE_BACK_WRITE_ALLOCATE               4'b1111
-`define AXI_AWCACHE_WRITE_BACK_READ_AND_WRITE_ALLOCATE      4'b1111
 
 `define AXI_SIZE_BYTES_1                                    3'b000                //突发宽度一个数据的宽度
 `define AXI_SIZE_BYTES_2                                    3'b001
@@ -51,72 +31,84 @@ module ysyx_040066 # (
     parameter AXI_DATA_WIDTH    = 64,
     parameter AXI_ADDR_WIDTH    = 32,
     parameter AXI_ID_WIDTH      = 4,
-    parameter AXI_STRB_WIDTH    = AXI_DATA_WIDTH/8,
-    parameter AXI_USER_WIDTH    = 1
+    parameter AXI_STRB_WIDTH    = AXI_DATA_WIDTH/8
 )(
     input                               clock,
     input                               reset,
+    input                               io_interrupt,
 
-/*	input                               rw_valid_i,         //IF&MEM输入信号
-	output                              rw_ready_o,         //IF&MEM输入信号
-    output reg [RW_DATA_WIDTH-1:0]      data_read_o,        //IF&MEM输入信号
-    input  [RW_DATA_WIDTH-1:0]          rw_w_data_i,        //IF&MEM输入信号
-    input  [RW_ADDR_WIDTH-1:0]          rw_addr_i,          //IF&MEM输入信号
-    input  [7:0]                        rw_size_i,          //IF&MEM输入信号*/
+    // AXI-master
+    input                               io_master_awready,              
+    output                              io_master_awvalid,
+    output [AXI_ADDR_WIDTH-1:0]         io_master_awaddr,
+    output [AXI_ID_WIDTH-1:0]           io_master_awid,
+    output [7:0]                        io_master_awlen,
+    output [2:0]                        io_master_awsize,
+    output [1:0]                        io_master_awburst,
 
-
-
-    // Advanced eXtensible Interface
-    input                               axi_aw_ready_i,              
-    output                              axi_aw_valid_o,
-    output [AXI_ADDR_WIDTH-1:0]         axi_aw_addr_o,
-    output [2:0]                        axi_aw_prot_o,
-    output [AXI_ID_WIDTH-1:0]           axi_aw_id_o,
-    output [AXI_USER_WIDTH-1:0]         axi_aw_user_o,
-    output [7:0]                        axi_aw_len_o,
-    output [2:0]                        axi_aw_size_o,
-    output [1:0]                        axi_aw_burst_o,
-    output                              axi_aw_lock_o,
-    output [3:0]                        axi_aw_cache_o,
-    output [3:0]                        axi_aw_qos_o,
-    output [3:0]                        axi_aw_region_o,
-
-    input                               axi_w_ready_i,                
-    output                              axi_w_valid_o,
-    output [AXI_DATA_WIDTH-1:0]         axi_w_data_o,
-    output [AXI_DATA_WIDTH/8-1:0]       axi_w_strb_o,
-    output                              axi_w_last_o,
-    output [AXI_USER_WIDTH-1:0]         axi_w_user_o,
+    input                               io_master_wready,                
+    output                              io_master_wvalid,
+    output [AXI_DATA_WIDTH-1:0]         io_master_wdata,
+    output [AXI_DATA_WIDTH/8-1:0]       io_master_wstrb,
+    output                              io_master_wlast,
     
-    output                              axi_b_ready_o,                
-    input                               axi_b_valid_i,
-    input  [1:0]                        axi_b_resp_i,                 
-    input  [AXI_ID_WIDTH-1:0]           axi_b_id_i,
-    input  [AXI_USER_WIDTH-1:0]         axi_b_user_i,
+    output                              io_master_bready,                
+    input                               io_master_bvalid,
+    input  [1:0]                        io_master_bresp,                 
+    input  [AXI_ID_WIDTH-1:0]           io_master_bid,
 
-    input                               axi_ar_ready_i,                
-    output                              axi_ar_valid_o,
-    output [AXI_ADDR_WIDTH-1:0]         axi_ar_addr_o,
-    output [2:0]                        axi_ar_prot_o,
-    output [AXI_ID_WIDTH-1:0]           axi_ar_id_o,
-    output [AXI_USER_WIDTH-1:0]         axi_ar_user_o,
-    output [7:0]                        axi_ar_len_o,
-    output [2:0]                        axi_ar_size_o,
-    output [1:0]                        axi_ar_burst_o,
-    output                              axi_ar_lock_o,
-    output [3:0]                        axi_ar_cache_o,
-    output [3:0]                        axi_ar_qos_o,
-    output [3:0]                        axi_ar_region_o,
+    input                               io_master_arready,                
+    output                              io_master_arvalid,
+    output [AXI_ADDR_WIDTH-1:0]         io_master_araddr,
+    output [AXI_ID_WIDTH-1:0]           io_master_arid,
+    output [7:0]                        io_master_arlen,
+    output [2:0]                        io_master_arsize,
+    output [1:0]                        io_master_arburst,
     
-    output                              axi_r_ready_o,                 
-    input                               axi_r_valid_i,                
-    input  [1:0]                        axi_r_resp_i,
-    input  [AXI_DATA_WIDTH-1:0]         axi_r_data_i,
-    input                               axi_r_last_i,
-    input  [AXI_ID_WIDTH-1:0]           axi_r_id_i,
-    input  [AXI_USER_WIDTH-1:0]         axi_r_user_i
+    output                              io_master_rready,                 
+    input                               io_master_rvalid,                
+    input  [1:0]                        io_master_rresp,
+    input  [AXI_DATA_WIDTH-1:0]         io_master_rdata,
+    input                               io_master_rlast,
+    input  [AXI_ID_WIDTH-1:0]           io_master_rid,
+
+    //AXI_slave
+    output io_slave_awready,
+    input io_slave_awvalid,
+    input [AXI_ADDR_WIDTH-1:0] io_slave_awaddr,
+    input [AXI_ID_WIDTH-1:0] io_slave_awid,
+    input [7:0] io_slave_awlen,
+    input [2:0] io_slave_awsize,
+    input [1:0] io_slave_awburst,
+
+    output io_slave_wready,
+    input io_slave_wvalid,
+    input [AXI_DATA_WIDTH-1:0] io_slave_wdata,
+    input [AXI_DATA_WIDTH/8-1:0] io_slave_wstrb,
+    input io_slave_wlast,
+
+    input io_slave_bready,
+    output io_slave_bvalid,
+    output [1:0] io_slave_bresp,
+    output [AXI_ID_WIDTH-1:0] io_slave_bid,
+
+    output io_slave_arready,
+    input io_slave_arvalid,
+    input [AXI_ADDR_WIDTH-1:0] io_slave_araddr,
+    input [AXI_ID_WIDTH-1:0] io_slave_arid,
+    input [7:0] io_slave_arlen,
+    input [2:0] io_slave_arsize,
+    input [1:0] io_slave_arburst,
 );
     
+    assign io_slave_awready=0;
+    assign io_slave_wready=0;
+    assign io_slave_bvalid=0;
+    assign io_slave_bresp=2'b11;
+    assign io_slave_bid={AXI_ID_WIDTH{1'b0}};
+    assign io_slave_arready=0;
+    
+
     reg [511:0] rd_data;
     reg [511:0] ins_data;
     wire [511:0] wr_data;
@@ -158,73 +150,57 @@ module ysyx_040066 # (
     always @(posedge clk)begin
         if(rst) {ins_available,rd_available}<=2'b11;
         else begin
-            if(is_ins&&axi_ar_ready_i) ins_available<=1'b0;
+            if(is_ins&&io_master_arready) ins_available<=1'b0;
             else if(ins_done) ins_available<=1'b1;
 
-            if(is_rd&&axi_ar_ready_i) rd_available<=1'b0;
+            if(is_rd&&io_master_arready) rd_available<=1'b0;
             else if(rd_done) rd_available<=1'b1;
         end
     end
 
-    assign ins_done=(axi_r_valid_i&&axi_r_last_i&&axi_r_id_i[0]);
-    assign rd_done=(axi_r_valid_i&&axi_r_last_i&&~axi_r_id_i[0]);
+    assign ins_done=(io_master_rvalid&&io_master_rlast&&io_master_rid[0]);
+    assign rd_done=(io_master_rvalid&&io_master_rlast&&~io_master_rid[0]);
     
     reg [2:0] count;
     always @(posedge clk) begin
         if(rst) count<=3'b0;
-        else if(wr_burst&&wr_req&&axi_w_ready_i) count<=count+3'b1;
+        else if(wr_burst&&wr_req&&io_master_wready) count<=count+3'b1;
         else count<=3'b0;
     end
 
     // ------------------Write Transaction------------------
-    parameter AXI_SIZE      = $clog2(AXI_DATA_WIDTH / 8);
     wire [AXI_ID_WIDTH-1:0] axi_id              = {AXI_ID_WIDTH{1'b0}};
-    wire [AXI_USER_WIDTH-1:0] axi_user          = {AXI_USER_WIDTH{1'b0}};
-    wire [7:0] axi_len      =  8'b0 ;
-    wire [2:0] axi_size     = AXI_SIZE[2:0];
     // 写地址通道  以下没有备注初始化信号的都可能是你需要产生和用到的
-    assign axi_aw_valid_o   = wr_req;
-    assign axi_aw_addr_o    = wr_addr[31:0];
-    assign axi_aw_prot_o    = `AXI_PROT_UNPRIVILEGED_ACCESS | `AXI_PROT_SECURE_ACCESS | `AXI_PROT_DATA_ACCESS;  //初始化信号即可
-    assign axi_aw_id_o      = axi_id;                                                                           //初始化信号即可
-    assign axi_aw_user_o    = axi_user;                                                                         //初始化信号即可
-    assign axi_aw_len_o     = wr_burst?8'h7:8'b0;
-    assign axi_aw_size_o    = wr_burst?`AXI_SIZE_BYTES_64:wr_len;
-    assign axi_aw_burst_o   = wr_burst?`AXI_BURST_TYPE_INCR:`AXI_BURST_TYPE_FIXED;                                                             
-    assign axi_aw_lock_o    = 1'b0;                                                                             //初始化信号即可
-    assign axi_aw_cache_o   = `AXI_AWCACHE_WRITE_BACK_READ_AND_WRITE_ALLOCATE;                                  //初始化信号即可
-    assign axi_aw_qos_o     = 4'h0;                                                                             //初始化信号即可
-    assign axi_aw_region_o  = 4'h0;                                                                             //初始化信号即可
+    assign io_master_awvalid   = wr_req;
+    assign io_master_awaddr    = wr_addr[31:0];
+    assign io_master_awid      = axi_id;                                                                      //初始化信号即可
+    assign io_master_awlen     = wr_burst?8'h7:8'b0;
+    assign io_master_awsize    = wr_burst?`AXI_SIZE_BYTES_64:wr_len;
+    assign io_master_awburst   = `AXI_BURST_TYPE_INCR;                                                          //初始化信号即可
 
     // 写数据通道
-    assign axi_w_valid_o    = wr_req;
-    assign axi_w_data_o     = wr_data[{count,3'h7}:{count,3'h0}] ;
-    assign axi_w_strb_o     = wr_burst?8'hff:wr_mask;
-    assign axi_w_last_o     = wr_burst||(count==3'h7);
-    assign axi_w_user_o     = axi_user;                                                                         //初始化信号即可
+    assign io_master_wvalid    = wr_req;
+    assign io_master_wdata     = wr_data[{count,3'h7}:{count,3'h0}] ;
+    assign io_master_wstrb     = wr_burst?8'hff:wr_mask;
+    assign io_master_wlast     = wr_burst||(count==3'h7);                                                    //初始化信号即可
 
     // 写应答通道
-    assign axi_b_ready_o    = 1'b1;
+    assign io_master_bready    = 1'b1;
 
     // ------------------Read Transaction------------------
 
     // Read address channel signals
-    assign axi_ar_valid_o   = ins_available||rd_available;
-    assign axi_ar_addr_o    = ins_available?ins_addr:rd_addr;
-    assign axi_ar_prot_o    = `AXI_PROT_UNPRIVILEGED_ACCESS | `AXI_PROT_SECURE_ACCESS | `AXI_PROT_DATA_ACCESS;  //初始化信号即可
-    assign axi_ar_id_o      = {(AXI_ID_WIDTH-1){ins_available}};                                                                           //初始化信号即可                        
-    assign axi_ar_user_o    = axi_user;                                                                         //初始化信号即可
-    assign axi_ar_len_o     = (ins_available&&ins_burst||rd_available&&rd_burst)?8'h7:8'h0;                                                                          
-    assign axi_ar_size_o    = ins_available?(ins_burst?`AXI_SIZE_BYTES_64:`AXI_SIZE_BYTES_32):(rd_burst?`AXI_SIZE_BYTES_64:rd_len);
-    assign axi_ar_burst_o   = (ins_available&&ins_burst||rd_available&&rd_burst)?`AXI_BURST_TYPE_INCR:`AXI_BURST_TYPE_FIXED;
-    assign axi_ar_lock_o    = 1'b0;                                                                             //初始化信号即可
-    assign axi_ar_cache_o   = `AXI_ARCACHE_NORMAL_NON_CACHEABLE_NON_BUFFERABLE;                                 //初始化信号即可
-    assign axi_ar_qos_o     = 4'h0;                                                                             //初始化信号即可
-
+    assign io_master_arvalid   = ins_available||rd_available;
+    assign io_master_araddr    = ins_available?ins_addr:rd_addr;
+    assign io_master_arid      = {(AXI_ID_WIDTH-1){ins_available}};                                                                           //初始化信号即可                        
+    assign io_master_arlen     = (ins_available&&ins_burst||rd_available&&rd_burst)?8'h7:8'h0;                                                                          
+    assign io_master_arsize    = ins_available?(ins_burst?`AXI_SIZE_BYTES_64:`AXI_SIZE_BYTES_32):(rd_burst?`AXI_SIZE_BYTES_64:rd_len);
+    assign io_master_arburst   = (ins_available&&ins_burst||rd_available&&rd_burst)?`AXI_BURST_TYPE_INCR:`AXI_BURST_TYPE_FIXED;
+    
     // Read data channel signals
-    assign axi_r_ready_o    = 1'b1;
+    assign io_master_rready    = 1'b1;
 
-    always @(posedge clk) if(axi_r_valid_i) begin
+    always @(posedge clk) if(io_master_rvalid) begin
         
     end
 
