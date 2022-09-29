c1585359c3222fc38fc0329b58b4338e35fad489
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-29 11:15:59 +0800

    >  sim RTL ysyx_220066 曹云帆 Linux cao-VirtualBox 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:15:59 up  1:28,  1 user,  load average: 1.61, 1.35, 1.17

--- npc/csrc/src/mainloop.cpp
+++ npc/csrc/src/mainloop.cpp
@@ -37,7 +37,7 @@ static inline void add_ar(){
   read_table[pos].id=mycpu->io_master_arid;
   read_table[pos].addr=mycpu->io_master_araddr;
   read_table[pos].left=mycpu->io_master_arlen+1;
-  if(read_table[pos].left>>31u){
+  if(read_table[pos].left>=0x80000000u){
     Assert(read_table[pos].left==8,"Read should be cached! %x",read_table[pos].addr);
   }else{
     Assert(read_table[pos].left==1,"Read should not be cached! %x",read_table[pos].addr);
@@ -49,7 +49,7 @@ static inline void add_aw(){
   mycpu->io_master_awready=1;
   write_table.left=mycpu->io_master_awlen+1;
   write_table.addr=mycpu->io_master_awaddr;
-  if(write_table.addr>>31u){
+  if(write_table.addr>=0x80000000u){
     Assert(write_table.left==8,"Write should be cached! %x",write_table.addr);
   }else{
     Assert(write_table.left==1,"Write should be uncached! %x",write_table.addr);
