51f25d343ec2fb611e1f3d3ae46d1967eba19a50
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-29 11:05:14 +0800

    >  sim RTL ysyx_220066 曹云帆 Linux cao-VirtualBox 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:05:14 up  1:17,  1 user,  load average: 1.27, 0.95, 0.94

--- npc/csrc/src/mainloop.cpp
+++ npc/csrc/src/mainloop.cpp
@@ -66,18 +66,20 @@ static inline void deal_r(){
 
 static inline void deal_w(){
   if(!write_table.left){
+    Assert(!mycpu->io_master_wvalid,"should not write!");
     mycpu->io_master_wready=0;
     return;
   }
+  Assert(mycpu->io_master_wvalid,"should write!");
   mycpu->io_master_wready=1;
   u8 error;
   data_write(write_table.addr,&mycpu->io_master_wdata,mycpu->io_master_wstrb,&error);
+  write_table.addr+=64/8;
+  write_table.left--;
   if(!write_table.left) Assert(mycpu->io_master_wlast,"Write expect to be ended!");
   else Assert(!mycpu->io_master_wlast,"Write expect not to be ended!");
   write_table.error|=error;
   write_table.tag=(!write_table.left);
-  write_table.addr+=64/8;
-  write_table.left--;
 }
 
 static inline void deal_b(){
